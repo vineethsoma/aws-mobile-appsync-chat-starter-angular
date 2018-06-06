00a334ce657246b804e97ac7646fa289a935fe28
vineeth soma <vineeth.soma@esadvisory.com>
2018-06-06 09:52:37 -0400

    chat app working

---
+++ src/AppSync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://s3c2dil2ivdtxj57rbey6js7qu.appsync-api.us-east-1.amazonaws.com/graphql",
+    "region": "us-east-1",
+    "authenticationType": "AMAZON_COGNITO_USER_POOLS",
+    "apiKey": "null"
+}
